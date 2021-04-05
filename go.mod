b9136f9c391b50cf5006610cc0d195125d07e39f
kmiloparra <cristian.parra@mercadolibre.com.co>
2021-04-04 21:49:56 -0500

    pruebas moduls expose2 v3

--- go.mod
+++ go.mod
@@ -9,7 +9,8 @@ module github.com/kmiloparra/resource-mutant
 go 1.16
 
 //require (
-//	misProyectos/resourceMutant/constantes v0.0.0-00010101000000-000000000000 // indirect
-//	misProyectos/resourceMutant/utilidades v0.0.0-00010101000000-000000000000 // indirect
-//	misProyectos/resourceMutant/validaciones v0.0.0-00010101000000-000000000000 // indirect
+////	misProyectos/resourceMutant/constantes v0.0.0-00010101000000-000000000000 // indirect
+////	misProyectos/resourceMutant/utilidades v0.0.0-00010101000000-000000000000 // indirect
+////	misProyectos/resourceMutant/validaciones v0.0.0-00010101000000-000000000000 // indirect
+//	github.com/kmiloparra/resource-mutant
 //)
--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"misProyectos/resourceMutant/constantes"
-	"misProyectos/resourceMutant/utilidades"
-	"misProyectos/resourceMutant/validaciones"
-)
-
-func main() {
-	dna := []string{"GTCpAGTA", "TCGAGTAG", "CGAGTAGT", "CGAGTAGT", "pppppppp", "GAGTCGAT", "AGAGTCGT", "CGAGTAGT"}
-
-	fmt.Println(validaciones.ValidacionFilaVacia(dna))
-	fmt.Println(validaciones.ValidacionNxN(dna))
-	fmt.Println(validaciones.ValidacionDominio(dna))
-	fmt.Println(utilidades.PivotearMatrix(dna))
-	fmt.Println(utilidades.ObtenerDiagonales(dna))
-	fmt.Println(utilidades.EncontrarIncidenciasHash(utilidades.StringToMap(constantes.SECUENCIAS_GEN_MUTANTE_HASHMAP),
-		utilidades.StringToMap(constantes.SECUENCIAS_INVALIDAS),
-		"TTTTeAAAA"))
-
-}
+//import (
+//	"fmt"
+//	"misProyectos/resourceMutant/constantes"
+//	"misProyectos/resourceMutant/utilidades"
+//	"misProyectos/resourceMutant/validaciones"
+//)
+//
+//func main() {
+//	dna := []string{"GTCpAGTA", "TCGAGTAG", "CGAGTAGT", "CGAGTAGT", "pppppppp", "GAGTCGAT", "AGAGTCGT", "CGAGTAGT"}
+//
+//	fmt.Println(validaciones.ValidacionFilaVacia(dna))
+//	fmt.Println(validaciones.ValidacionNxN(dna))
+//	fmt.Println(validaciones.ValidacionDominio(dna))
+//	fmt.Println(utilidades.PivotearMatrix(dna))
+//	fmt.Println(utilidades.ObtenerDiagonales(dna))
+//	fmt.Println(utilidades.EncontrarIncidenciasHash(utilidades.StringToMap(constantes.SECUENCIAS_GEN_MUTANTE_HASHMAP),
+//		utilidades.StringToMap(constantes.SECUENCIAS_INVALIDAS),
+//		"TTTTeAAAA"))
+//
+//}
